b564b89276d2bd508b709ecb937bbdcdd95b489e
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-03-29 13:01:42 +0300

    [Feature] Serve static files from /public

---
+++ index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const path = require('path');
+
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World from Docker');
+  console.log('Hello World from Docker');
+});
+
+app.get('/home', (req,res) => {
+  res.send('Welcome Home From Docker');
+});
+
+*/
+app.use('/static', express.static(path.join(__dirname, 'public'))); // eslint-disable-line
+// Html file served at http://localhost:4000/static/
+
+app.listen(PORT, () => {
+  console.log('Server listening on port ', PORT);
+});
